3a3e1b85a3d33f37f0a01d405d09bb5db1fe21d1
protege47007 <davidak.a@yahoo.com>
2022-05-04 19:55:38 +0100

    speaker service getList and getListShort added

--- services/speakerService.js
+++ services/speakerService.js
@@ -1,5 +1,6 @@
 const fs =require("fs")
 const { get } = require("http")
+const { title } = require("process")
 const util = require("util")
 
 // to use async/await with fs.readFile, util.promisify gives us that
@@ -18,10 +19,69 @@ class Speaker{
         return data.map(speaker => ({name: speaker.name, shortName: speaker.shortName}))
     }
 
-    async getArtworks(){
+    async getAllArtwork(){
         const data = await this.getData()
 
+        // array.reduce is used to traverse all speaker elements and create an array 
+        // that contains all artworks
+        const artwork = data.reduce( (prev, curr) => {
+            if(curr.artwork){
+                prev = [...prev, ...curr.artwork]
 
+            }
+            return acc
+        }, [])
+
+        return artwork
+
+    }
+
+    async getArtworkForSpeaker(shortname){
+        const data = await this.getData()
+        
+        const speaker = data.find(speaker => {
+            if(speaker.shortName === shortname) return speaker
+        })
+
+        if(!speaker || !speaker.artwork) return null
+
+        return speaker.artwork
+    }
+    
+    async getSpeaker(shortname){
+        const data = await this.getData()
+        
+        const speaker = data.find(speaker => {
+            if(speaker.shortName === shortname) return speaker
+        })
+
+        if(!speaker) return null
+
+        return {
+            title: speaker.title,
+            name: speaker.name,
+            description: speaker.description,
+            shortname: speaker.shortName
+        }
+    }
+
+    async getListShort(){
+        const data = await this.getData()
+        return data.map(speaker => ({
+            name: speaker.name,
+            shortname: speaker.shortName,
+            title: speaker.title
+        }))
+    }
+
+    async getList(){
+        const data = await this.getData()
+        return data.map(speaker => ({
+            name: speaker.name,
+            shortname: speaker.shortName,
+            title: speaker.title,
+            summary: speaker.summary
+        }))
     }
 
     async getData(){
