431945c7e6860cf3a4fd303880f492412907e8a1
protege47007 <davidak.a@yahoo.com>
2022-05-05 03:04:04 +0100

    complete feedback post route

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const path = require("path")
 const createError = require("http-errors")
+const cookieSession = require("cookie-session")
 
 const FeedbackService = require("./services/feedbackService")
 const SpeakerService = require("./services/speakerService")
 
-// initialising the data services
+// initializing the data services
 const feedbackService = new FeedbackService("./data/feedback.json")
 const speakerService = new SpeakerService("./data/speakers.json")
 
@@ -14,16 +15,24 @@ const routes = require("./routes")
 const app = express()
 const port = 3000
 
-app.set("view engine", "ejs")
-app.set("views", "public")
-
 app.locals.siteName = "Art Meetups" // Global variable that is available throughout the app 
 
+
+app.use(cookieSession({
+    name: "session",
+    keys: [process.env.KEY_1, process.env.KEY_2]
+}))
+app.set("view engine", "ejs")
+app.set("views", "public")
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
 app.use(async (req, res, next) => {
     try {
         const names = await speakerService.getNames()
         // this is also a global variable but it attached to every response object
-        // unlike app.locals vars, this refetches its data. useful to keep track of 
+        // unlike app.locals vars, this re-fetches its data. useful to keep track of 
         // changes in our database to our global variables
         res.locals.speakerNames = names
         return next()
--- routes/feedback.js
+++ routes/feedback.js
@@ -1,18 +1,46 @@
 const express = require("express")
+const {check, validationResult} = require("express-validator")
 const router = express.Router()
+
+// sanitizing our form inputs from injection attacks and ensuring we have data to operate with
+const validationConstraints = [
+    check("name").trim.isLength({min: 3}).escape().withMessage("A name is required"),
+    check("email").trim.isEmail().normalizeEmail().withMessage("A valid email is required"),
+    check("title").trim.isLength({min: 5}).escape().withMessage("A title is required"),
+    check("message").trim.isLength({min: 5}).escape().withMessage("A message is required"),
+]
 
 module.exports = ({feedbackService}) => {
     
     router.get("/", async (req, res, next) => {
         try {
             const feedbacks = await feedbackService.getList()
-            res.json({message: "this is the feedback 's index page", feedbacks})
+            const errors = req.session.feedback? req.session.feedback.errors : false
+            const successMsg = req.session.feedback? req.session.feedback.message : false
+            req.session.feedback = {}
+
+            res.json({message: "this is the feedback 's index page", feedbacks, errors, successMsg})
         } catch (error) {
             return next(error)
         }
     })
 
-    router.post("/", (req, res) => {
+    router.post("/", validationConstraints, async (req, res, next) => {
+        const errors = validationResult(req) //this checks for errors from the constraints
+        
+        if(!errors.isEmpty()){ // if there are errors from the req.body validation constraints
+            req.session.feedback = {
+                errors: errors.array() // returns an array of errors
+            }
+
+            return res.redirect("/feedback")
+        }
+
+        const {name, email, title, message} = req.body
+        await feedbackService.addEntry(name, email, title, message)
+        req.session.feedback = {
+            message: "Thank you for your feedback!"
+        }
         res.json({message: "message received!"})
     })
 
